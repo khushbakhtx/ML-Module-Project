37b0927f2e995394aeb7a2b5455130256b20c600
Khushbakht <khushbakht.dev@gmail.com>
2024-06-03 08:13:05 +0500

    First

--- model.py
+++ model.py
@@ -10,23 +10,22 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.metrics import roc_auc_score, roc_curve, auc, accuracy_score, mean_squared_error
 
-data = pd.read_csv("/datasets/data.csv")
+data = pd.read_csv("datasets/imon_international_dataset_cleaned.csv")
+data = data.drop(['Unnamed: 0'], axis=1)
+data['Просрочки за цикл (кумулятивный) [for classification]'] = (data['Просрочки за цикл (кумулятивный)'] < 30).astype(int)
+X = data.drop(['Просрочки за цикл (кумулятивный) [for classification]', 'Просрочки за цикл (кумулятивный)'], axis=1)
+y_classification = data['Просрочки за цикл (кумулятивный) [for classification]']
+X_train, X_test, y_train, y_test = train_test_split(X, y_classification, test_size = 0.3, random_state = 0)
 
-X = data.drop(['NObeyesdad', 'NObeyesdad_encoded'], axis=1)
-X = X.drop(['Unnamed: 0'], axis=1)
-y = data['NObeyesdad_encoded']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-rfc_model = RandomForestClassifier(criterion='gini', n_estimators=100, random_state=1, n_jobs=None)
+rfc_model = RandomForestClassifier(criterion='gini',
+                                 n_estimators=41,
+                                 random_state=None,
+                                 n_jobs=None)
 rfc_model.fit(X_train, y_train)
-
 y_pred_rfc = rfc_model.predict(X_test)
-y_prob_rfc = rfc_model.predict_proba(X_test)[:, 1]
-
 score = rfc_model.score(X_test, y_test)
 print(f"Accuracy: {round(score*100, 1)}%")
 
-# import joblib
-# joblib.dump(rfc_model, "obesity_model.joblib")
-# print(X.columns)
+import joblib
+joblib.dump(rfc_model, "imon_international_model.joblib")
+print(X.columns)
--- pages/Imon-International.py
+++ pages/Imon-International.py
@@ -4,20 +4,27 @@ import pandas as pd
 import pickle
 import joblib
 
-one = open("models/imon.pkl", "rb")
-model = joblib.load(one)
+model = joblib.load("models/imon_international_model.joblib")
 
-def predict():
+def predict(age, old_client, credit_nominal, deadline, gender):
     pass
 
 def main():
     html_temp = """
     <div style="background-color:#93d;padding:10px;border-radius:5px">
         <h2 style="color:white;text-align:center;">Credit Scoring Approval</h2>
-    </div>
+    </div><br>
     """
+    st.caption("Imon-International")
     st.markdown(html_temp, unsafe_allow_html=True)
 
+    age = st.number_input("Возраст", min_value=18, max_value=90)
+    old = st.radio("Вы повторный клиент", options=["Да", "Нет"], key="old_client")
+    if old == "Да":
+        old_client = 1
+    elif old == "Нет":
+        old_client = 0
 
+    st.write("Accuracy of the model: ", 95.9, "%")
 if __name__=='__main__':
     main()
